106cf6362f8106ab9bc5d7389feca3a430a15f1a
ryoshindo <udoryo999@gmail.com>
2023-03-10 01:13:16 +0900

    feat: add http_config

---
+++ http_config/src/lib.rs
@@ -0,0 +1,49 @@
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+
+proxy_wasm::main! {{
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> {
+        Box::new(HttpConfigHeaderRoot {
+            header_content: String::new(),
+        })
+    });
+}}
+
+struct HttpConfigHeader {
+    header_content: String,
+}
+
+impl Context for HttpConfigHeader {}
+
+impl HttpContext for HttpConfigHeader {
+    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
+        self.add_http_response_header("custom-header", self.header_content.as_str());
+        Action::Continue
+    }
+}
+
+struct HttpConfigHeaderRoot {
+    header_content: String,
+}
+
+impl Context for HttpConfigHeaderRoot {}
+
+impl RootContext for HttpConfigHeaderRoot {
+    fn on_configure(&mut self, _: usize) -> bool {
+        if let Some(config_bytes) = self.get_plugin_configuration() {
+            self.header_content = String::from_utf8(config_bytes).unwrap()
+        }
+        true
+    }
+
+    fn create_http_context(&self, _: u32) -> Option<Box<dyn HttpContext>> {
+        Some(Box::new(HttpConfigHeader {
+            header_content: self.header_content.clone(),
+        }))
+    }
+
+    fn get_type(&self) -> Option<ContextType> {
+        Some(ContextType::HttpContext)
+    }
+}
